306b31b9eb1f658f92d298667d26d1cbd7233dbc
lmelo <leonardomelosrj@gmail.com>
2022-07-01 19:04:50 -0300

    Atividade finalizada de ingles

---
+++ script.js
@@ -0,0 +1,31 @@
+
+let comedy = [
+    {
+    title: "FreeGuy",
+    director: "ShawnLevy",
+    year:2021
+    }
+]
+
+let action = [
+    {
+    title: "Morbius", 
+    director: "DanielEspinosa",
+    year: 2022
+    }
+]
+
+let fantasy = [
+    {
+    title: "Ghostbusters",
+    director: "JasonReitman", 
+    year: 2021
+    }
+]
+
+if( action != comedy)
+{
+    console.log("my favorite movie is action")
+}else{
+    console.log("not even 5 stars")
+}
